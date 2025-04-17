da51be1d100674aa7d2ad728cf42347e58365522
HadiAlmayyad <s202041000@kfupm.edu.sa>
2025-04-17 17:18:55 +0300

    Initialise event management page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyEventsPage from "./pages/MyEvents/MyEventsPage";
+import EventManagePage from "./pages/EventManage/EventManagePage";
 
 function App() {
   return (
    <Router>
     <Routes>
       <Route path="/" element={<MyEventsPage />} />
+      <Route path="/event-manage" element={<EventManagePage/>} />
     </Routes>
    </Router>
   );
---
+++ src/pages/EventManage/EventManagePage.js
@@ -0,0 +1,39 @@
+import "./EventManagePage.css";
+import React, { useState } from "react";
+import { Button, Modal, Placeholder } from "react-bootstrap";
+import NavbarComponent from "../../components/MyEventsPageComponents/NavbarComponent";
+
+function EventManage()
+{
+	const [ showPreview, setShowPreview ] = useState( false );
+
+	const openPreview = function()
+	{
+		setShowPreview( true )
+	};
+
+	const closePreview = function()
+	{
+		setShowPreview( false )
+	};
+
+	return (
+		<div>
+			<NavbarComponent/>
+			<Modal show={showPreview} size="xl" onHide={closePreview}>
+				<Modal.Header closeButton={true}>
+					<Modal.Title>Preview</Modal.Title>
+				</Modal.Header>
+				<Placeholder as={Modal.Body} animation="wave">
+					<Placeholder xs={12}/>
+				</Placeholder>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={closePreview}>Exit Preview</Button> <Button>Submit</Button>
+				</Modal.Footer>
+			</Modal>
+			<Button variant="secondary">Cancel</Button> <Button onClick={openPreview} variant="outline-light">Preview</Button> <Button>Submit</Button>
+		</div>
+	);
+}
+
+export default EventManage;
