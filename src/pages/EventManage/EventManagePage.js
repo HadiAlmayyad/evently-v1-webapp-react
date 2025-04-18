3327c84e561dd065d0edb8ef75feba6bc32b7743
HadiAlmayyad <s202041000@kfupm.edu.sa>
2025-04-19 02:01:07 +0300

    Update EventManagePage.js
    Added input fields

--- src/pages/EventManage/EventManagePage.js
+++ src/pages/EventManage/EventManagePage.js
@@ -1,6 +1,6 @@
 import "./EventManagePage.css";
 import React, { useState } from "react";
-import { Button, Modal, Placeholder } from "react-bootstrap";
+import { Button, Container, FloatingLabel, Form, Modal, Placeholder } from "react-bootstrap";
 import NavbarComponent from "../../components/MyEventsPageComponents/NavbarComponent";
 
 function EventManagePage()
@@ -20,18 +20,52 @@ function EventManagePage()
 	return (
 		<div>
 			<NavbarComponent/>
-			<Modal show={showPreview} size="xl" onHide={closePreview}>
-				<Modal.Header closeButton={true}>
-					<Modal.Title>Preview</Modal.Title>
-				</Modal.Header>
-				<Placeholder as={Modal.Body} animation="wave">
-					<Placeholder xs={12}/>
-				</Placeholder>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={closePreview}>Exit Preview</Button> <Button>Submit</Button>
-				</Modal.Footer>
-			</Modal>
-			<Button variant="secondary">Cancel</Button> <Button onClick={openPreview} variant="outline-light">Preview</Button> <Button>Submit</Button>
+			<Container fluid="md">
+				<Form>
+					<Form.Group>
+						<Form.Label>Thumbnail</Form.Label>
+						<Form.Control type="file"/>
+						<Form.Text>Upload an image</Form.Text>
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<FloatingLabel label="Description">
+							<Form.Control as="textarea"/>
+						</FloatingLabel>
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Label>Location</Form.Label>
+						<Form.Select>
+							<option>Select location</option>
+						</Form.Select>
+						<Form.Text>Where will the event be held?</Form.Text>
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Label>Date</Form.Label>
+						<Form.Control placeholder="What day is the event taking place?"/>
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Label>Time</Form.Label>
+						<Form.Control placeholder="What time is the event taking place?"/>
+					</Form.Group>
+					<br/>
+					<Button variant="secondary">Cancel</Button> <Button onClick={openPreview} variant="outline-light">Preview</Button> <Button variant="primary">Submit</Button>
+				</Form>
+				<Modal show={showPreview} size="xl" onHide={closePreview}>
+					<Modal.Header closeButton={true}>
+						<Modal.Title>Preview</Modal.Title>
+					</Modal.Header>
+					<Placeholder as={Modal.Body} animation="wave">
+						<Placeholder xs={12}/>
+					</Placeholder>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={closePreview}>Exit Preview</Button> <Button variant="primary">Submit</Button>
+					</Modal.Footer>
+				</Modal>
+			</Container>
 		</div>
 	);
 }
